855d4b6a324d54725b2a806407a5c1ddd266901a
Khurshid Yuldoshev <xurshidyoldoshev4@gmail.com>
2024-06-27 18:30:20 +0500

    Input validations

--- Management.Service/DTOs/LoginPostModel.cs
+++ Management.Service/DTOs/LoginPostModel.cs
@@ -1,9 +1,11 @@
 ﻿using Management.Service.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Management.Service.DTOs;
 public record LoginPostModel
 {
     [ManagementEmailAttribute]
     public string Email { get; set; }
+    [MinLength(6),MaxLength(15)]
     public string Password { get; set; }
 }
--- Management.Service/DTOs/UserPostModel.cs
+++ Management.Service/DTOs/UserPostModel.cs
@@ -1,12 +1,16 @@
 ﻿using Management.Service.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Management.Service.DTOs;
 public record UserPostModel
 {
+    [MinLength(3),MaxLength(10)]
     public string FirstName { get; set; }
+    [MinLength(3),MaxLength(10)]
     public string LastName { get; set; }
 
     [ManagementEmailAttribute]
     public string Email { get; set; }
+    [MinLength(6),MaxLength(15)]
     public string Password { get; set; }
 }
